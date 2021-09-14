32203457788cc299aeb627330b09042b9e42b330
Phuong Cong <ncphuong@cmc.com.vn>
2021-09-14 09:02:46 +0700

    first commit

--- B2_NguyenCongPhuong/app/src/main/java/npc/com/b2_nguyencongphuong/DialView.java
+++ B2_NguyenCongPhuong/app/src/main/java/npc/com/b2_nguyencongphuong/DialView.java
@@ -7,127 +7,72 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import org.w3c.dom.Attr;
+
 public class DialView extends View {
 
-    private static int SELECTION_COUNT = 4;  // Total number of selections.
-    private float mWidth;                    // Custom view width.
-    private float mHeight;                   // Custom view height.
-    private Paint mTextPaint;                // For text in the view.
-    private Paint mDialPaint;                // For dial circle in the view.
-    private float mRadius;                   // Radius of the dial.
-    private int mActiveSelection;            // The active selection.
-    // String buffer for dial labels and float for ComputeXY result.
+    private static int SELECTION_COUNT = 4;
+    private float mWidth;
+    private float mHeight;
+    private Paint mTextPaint;
+    private Paint mDialPaint;
+    private float mRadius;
+    private int mActiveSelection;
     private final StringBuffer mTempLabel = new StringBuffer(8);
     private final float[] mTempResult = new float[2];
 
-    /**
-     * Standard constructor.
-     *
-     * @param context The Context the view is running in, through which it can
-     *                access the current theme, resources, etc.
-     */
     public DialView(Context context) {
         super(context);
         init();
     }
 
-    /**
-     * This constructor is called when a view is built from an XML file,
-     * supplying attributes that were specified in the XML file.
-     *
-     * @param context The Context the view is running in, through which it can
-     *                access the current theme, resources, etc.
-     * @param attrs   The attributes of the XML tag that is inflating the view.
-     */
-    public DialView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+    public DialView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
         init();
     }
 
-    /**
-     * This constructor is called to supply the default style.
-     *
-     * @param context      The Context the view is running in, through which
-     *                     it can access the current theme, resources, etc.
-     * @param attrs        The attributes of the XML tag inflating the view.
-     * @param defStyleAttr The default style attributes.
-     */
-    public DialView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public DialView(Context context, AttributeSet attributeSet, int defStyleAttr) {
+        super(context, attributeSet, defStyleAttr);
         init();
     }
 
-    /**
-     * Helper method to initialize instance variables. Called by constructors.
-     */
+
     private void init() {
-        // Paint styles used for rendering are created here. This
-        // is a performance optimization, since onDraw() is called
-        // for every screen refresh.
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setColor(Color.GRAY);
         mTextPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         mTextPaint.setTextAlign(Paint.Align.CENTER);
         mTextPaint.setTextSize(40f);
         mDialPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mDialPaint.setColor(Color.GRAY);
+        mDialPaint.setColor(Color.BLACK);
 
-        // Initialize current selection (where the dial's "indicator" is
-        // pointing).
         mActiveSelection = 0;
 
-        // Set up onClick listener for this view.
-        // Rotates between each of the different selection
-        // states on each click.
         setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Rotate selection forward to the next valid choice.
                 mActiveSelection = (mActiveSelection + 1) % SELECTION_COUNT;
-                // Set dial background color to green if selection is >= 1.
-                if (mActiveSelection >= 1) {
-                    mDialPaint.setColor(Color.GREEN);
-                } else {
-                    mDialPaint.setColor(Color.GRAY);
-                }
-                // Redraw the view.
+                if (mActiveSelection >= 1)
+                    mDialPaint.setColor(Color.RED);
+                else
+                    mDialPaint.setColor(Color.BLACK);
+
                 invalidate();
             }
         });
     }
 
-    /**
-     * This is called during layout when the size of this view has changed. If
-     * the view was just added to the view hierarchy, it is called with the old
-     * values of 0. The code determines the drawing bounds for the custom view.
-     *
-     * @param w    Current width of this view.
-     * @param h    Current height of this view.
-     * @param oldw Old width of this view.
-     * @param oldh Old height of this view.
-     */
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        // Calculate the radius from the width and height.
-        mWidth = w;
-        mHeight = h;
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        mWidth = width;
+        mHeight = height;
         mRadius = (float) (Math.min(mWidth, mHeight) / 2 * 0.8);
     }
 
-    /**
-     * Render view content: an outer grey circle to serve as the "dial",
-     * and a smaller black circle to server as the indicator.
-     * The position of the indicator is based on mActiveSelection.
-     *
-     * @param canvas the canvas on which the background will be drawn
-     */
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // Draw the dial.
         canvas.drawCircle(mWidth / 2, mHeight / 2, mRadius, mDialPaint);
-
-        // Draw the text labels.
         final float labelRadius = mRadius + 20;
         StringBuffer label = mTempLabel;
         for (int i = 0; i < SELECTION_COUNT; i++) {
@@ -139,7 +84,6 @@ public class DialView extends View {
             canvas.drawText(label, 0, label.length(), x, y, mTextPaint);
         }
 
-        // Draw the indicator mark
         final float markerRadius = mRadius - 35;
         float[] xyData = computeXYForPosition(mActiveSelection, markerRadius);
         float x = xyData[0];
@@ -147,19 +91,10 @@ public class DialView extends View {
         canvas.drawCircle(x, y, 20, mTextPaint);
     }
 
-    /**
-     * Compute the X/Y-coordinates for a label or indicator,
-     * given the position number and radius
-     * where the label should be drawn.
-     *
-     * @param pos Zero based position index
-     * @param radius Radius where label/indicator is to be drawn.
-     * @return 2-element array. Element 0 is X-coordinate, element 1 is Y-coordinate.
-     */
     private float[] computeXYForPosition(final int pos, final float radius) {
         float[] result = mTempResult;
-        Double startAngle = Math.PI * (9 / 8d);   // Angles are in radians.
-        Double angle = startAngle + (pos * (Math.PI / 4));
+        Double startAngle = Math.PI * (9 / 8d);
+        Double angle = startAngle + (pos + (Math.PI / 4));
         result[0] = (float) (radius * Math.cos(angle)) + (mWidth / 2);
         result[1] = (float) (radius * Math.sin(angle)) + (mHeight / 2);
         return result;
